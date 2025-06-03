7dab0f598dcf2fcde4505369acbad04d5caacf02
werto87 <huhulasmichinruhe@web.de>
2025-06-03 13:52:48 +0200

    version updated. removed duplicate functions

--- all/conanfile.py
+++ all/conanfile.py
@@ -17,15 +17,7 @@ class ConfuAlgorithm(ConanFile):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
 
     def requirements(self):
-        self.requires("boost/1.84.0")
-
-    def package(self):
-        copy(self, "*.h*", src=os.path.join(self.source_folder, "confu_algorithm"),
-             dst=os.path.join(self.package_folder, "include", "confu_algorithm"))
-
-    def requirements(self):
         self.requires("boost/1.85.0")
-
 
     def layout(self):
         cmake_layout(self, src_folder=self.name+"-"+str(self.version))
